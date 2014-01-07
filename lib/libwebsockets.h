1fcaae24a7163a20244850f3dba6269668ba3ddb
Michael Haberler <git@mah.priv.at>
2014-01-07 19:17:06 +0100

    define struct libwebsocket_pollargs

--- lib/libwebsockets.h
+++ lib/libwebsockets.h
@@ -183,6 +183,15 @@ enum libwebsocket_callback_reasons {
 	LWS_CALLBACK_UNLOCK_POLL,
 };
 
+// argument structure for all external poll related calls
+// passed in via 'in'
+struct libwebsocket_pollargs {
+    int fd;            // applicable file descriptor
+    int events;        // the new event mask
+    int prev_events;   // the previous event mask
+};
+
+
 #ifndef LWS_NO_EXTENSIONS
 enum libwebsocket_extension_callback_reasons {
 	LWS_EXT_CALLBACK_SERVER_CONTEXT_CONSTRUCT,
